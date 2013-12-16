1ff69f06fab6ec23fbde98c9a7e404d6ecbd9145
Almog Kurtser <almog.kurtser@klarna.com>
2013-12-16 23:19:59 +0200

    Use Node.querySelector instead of getElementBy

--- seller_recently_completed_listings.user.js
+++ seller_recently_completed_listings.user.js
@@ -3,7 +3,7 @@
 // @include http://www.ebay.co*/itm/*
 // ==/UserScript== 
 
-sellerId = document.getElementsByClassName("mbg-nw")[0].innerHTML
+sellerId = document.querySelector(".mbg-nw").innerHTML
 href = "http://www.ebay.com/sch/" + sellerId + "/m.html?_ipg&_from&_nkw&_armrs=1&rt=nc&LH_PrefLoc=0&LH_Complete=1"
 completedItemsDiv = '<div class="si-pd-a"><a href="' + href + '">Completed listings</a></div>'
 
