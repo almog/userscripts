891f834486ab51610a0d975a2d88fec7e429cf79
Almog Kurtser <almog.kurtser@klarna.com>
2013-12-16 23:20:40 +0200

    Match the added button to ebay's new layout

--- seller_recently_completed_listings.user.js
+++ seller_recently_completed_listings.user.js
@@ -5,7 +5,7 @@
 
 sellerId = document.querySelector(".mbg-nw").innerHTML
 href = "http://www.ebay.com/sch/" + sellerId + "/m.html?_ipg&_from&_nkw&_armrs=1&rt=nc&LH_PrefLoc=0&LH_Complete=1"
-completedItemsDiv = '<div class="si-pd-a"><a href="' + href + '">Completed listings</a></div>'
+completedItemsDiv = '<div class="si-pd-a si-pd-move"><a href="' + href + '">Completed listings</a></div>'
 
 sellerOtherItemsDiv = document.getElementsByClassName("si-bdg si-pd-eu")[0]
 sellerOtherItemsDiv.innerHTML += completedItemsDiv
