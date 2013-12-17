3c0f59b757dbe652dd839431a6f722c936680dbf
Almog Kurtser <almog.kurtser@klarna.com>
2013-12-17 19:08:11 +0200

    Works for a completed lisiting page as well

--- seller_recently_completed_listings.user.js
+++ seller_recently_completed_listings.user.js
@@ -5,7 +5,15 @@
 
 sellerId = document.querySelector(".mbg-nw").innerHTML
 href = "http://www.ebay.com/sch/" + sellerId + "/m.html?_ipg&_from&_nkw&_armrs=1&rt=nc&LH_PrefLoc=0&LH_Complete=1"
-completedItemsDiv = '<div class="si-pd-a si-pd-move"><a href="' + href + '">Completed listings</a></div>'
 
-sellerOtherItemsDiv = document.getElementsByClassName("si-bdg si-pd-eu")[0]
-sellerOtherItemsDiv.innerHTML += completedItemsDiv
+
+isOriginalListingPage = document.querySelector(".vi-cvip-prel5 a") ? false : true
+if (isOriginalListingPage) {
+  completedItemsDiv = '<div class="si-pd-a si-pd-move"><a href="' + href + '">Completed listings</a></div>'
+  sellerInfoDiv = document.querySelector(".si-bdg.si-pd-eu")
+}
+else {
+  completedItemsDiv = '<span class="watchPipe " style="display: inline;padding-left:0px;">|</span> <div style="display: inline"><a href="' + href + '">Completed listings</a></div>'
+  sellerInfoDiv = document.querySelector(".si-content.si-mini-sinfo")
+}
+sellerInfoDiv.innerHTML += completedItemsDiv
