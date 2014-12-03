dab51e3a8390d94aa1a756b6f1bd577bbd30296d
Almog Kurtser <almog.kurtser@klarna.com>
2014-12-03 16:37:51 +0200

    Adds Yad2 contact form auto filler

---
+++ yad2_contact_details_filler_bookmarklet.js
@@ -0,0 +1,8 @@
+javascript:(function(){
+  $("#fromName").val("Your name");
+  %20%20%20$("#fromPhone").val("Phone number");
+  %20%20%20$("#fromEmail").val("");
+  %20%20%20$("#notes").val("Your message goes here");
+  %20
+}());
+
